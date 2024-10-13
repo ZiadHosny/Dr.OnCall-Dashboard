8ea99165326ff2711ed279df10fc1880621cd1db
Ziad Hosny <ziadhosny007@gmail.com>
2024-10-13 16:08:36 +0300

    add symptom pagination

--- src/components/Symptom/Symptoms.tsx
+++ src/components/Symptom/Symptoms.tsx
@@ -1,61 +1,106 @@
-import { mdiEye, mdiTrashCan } from '@mdi/js'
-import { useDoctors } from '../../hooks/doctor'
+import { mdiEye, mdiTrashCan, mdiPencil } from '@mdi/js'
 import Button from '../../common/Button'
 import Buttons from '../../common/Buttons'
-import ZTable from "../Table/Table"
-import UserAvatar from '../UserAvatar'
+import ZTable from '../Table/Table'
 import { useSymptoms } from '../../hooks/symptom'
+import { useState } from 'react'
+import { EditSymptom } from './EditSymptom'
+import { DeleteSymptom } from './DeleteSymptom'
+import { DialogDetails } from '../../common/Dialog/DialogDetails'
+import { SymptomType } from './symptom.interface'
 
-const tableHeads = [
-    'Symptom In English',
-    'Symptom In Arabic',
-    'CreatedAt'
-]
-
-const perPage = 5
-
+const tableHeads = ['Symptom In English', 'Symptom In Arabic', 'Created At']
 
 export const SymptomsTable = () => {
-    const { symptoms } = useSymptoms()
+  const [page, setPage] = useState(1)
+  const [limit, _setLimit] = useState(10)
+  const { symptoms, paginationInfo } = useSymptoms({ limit, page })
+  const [isModalInfoActive, setIsModalInfoActive] = useState(false)
+  const [isModalTrashActive, setIsModalTrashActive] = useState(false)
+  const [isModalEditActive, setIsModalEditActive] = useState(false)
+  const [currentSymptom, setCurrentSymptom] = useState<SymptomType>(null)
 
-    const TableBody = () => {
-        return (
-            <>
-                {
-                    symptoms.map((symptom) => (
-                        <tr key={symptom._id}>
-                            <td className="border-b-0 lg:w-6 before:hidden">
-                                <UserAvatar username={symptom.symptomEn} className="w-24 h-24 mx-auto lg:w-6 lg:h-6" />
-                            </td>
-                            <td data-label="symptomEn">{symptom.symptomEn}</td>
-                            <td data-label="symptomAr">{symptom.symptomAr}</td>
-                            <td data-label="CreatedAt"> {symptom.createdAt} </td>
-
-                            <td className="before:hidden lg:w-1 whitespace-nowrap">
-                                <Buttons type="justify-start lg:justify-end" noWrap>
-                                    <Button
-                                        color="info"
-                                        icon={mdiEye}
-                                        onClick={() => setIsModalInfoActive(true)}
-                                        small
-                                    />
-                                    <Button
-                                        color="danger"
-                                        icon={mdiTrashCan}
-                                        // onClick={() => setIsModalTrashActive(true)}
-                                        small
-                                    />
-                                </Buttons>
-                            </td>
-                        </tr>
-                    ))
-                }
-            </>
-        )
-    }
-
-
+  const TableBody = () => {
     return (
-        <ZTable tableHeads={tableHeads} perPage={perPage} tableBody={<TableBody />} />
+      <>
+        {currentSymptom && (
+          <EditSymptom
+            isActive={isModalEditActive}
+            setIsActive={setIsModalEditActive}
+            values={currentSymptom}
+          />
+        )}
+        {currentSymptom && (
+          <DeleteSymptom
+            isActive={isModalTrashActive}
+            setIsActive={setIsModalTrashActive}
+            values={currentSymptom}
+          />
+        )}
+        {currentSymptom && (
+          <DialogDetails
+            title="Symptom Details"
+            isActive={isModalInfoActive}
+            setIsActive={setIsModalInfoActive}
+            values={{
+              Id: currentSymptom._id,
+              'Symptom In English': currentSymptom.symptomEn,
+              'Symptom In Arabic': currentSymptom.symptomAr,
+              'Created At': currentSymptom.createdAt,
+            }}
+          />
+        )}
+
+        {symptoms?.map((symptom) => (
+          <tr key={symptom._id}>
+            <td data-label="symptomEn">{symptom.symptomEn}</td>
+            <td data-label="symptomAr">{symptom.symptomAr}</td>
+            <td data-label="CreatedAt">{symptom.createdAt}</td>
+
+            <td className="before:hidden lg:w-1 whitespace-nowrap">
+              <Buttons type="justify-start lg:justify-end" noWrap>
+                <Button
+                  color="info"
+                  icon={mdiEye}
+                  onClick={() => {
+                    setIsModalInfoActive(true)
+                    setCurrentSymptom(symptom)
+                  }}
+                  small
+                />
+                <Button
+                  color="warning"
+                  icon={mdiPencil}
+                  onClick={() => {
+                    setIsModalEditActive(true)
+                    setCurrentSymptom(symptom)
+                  }}
+                  small
+                />
+                <Button
+                  color="danger"
+                  icon={mdiTrashCan}
+                  onClick={() => {
+                    setIsModalTrashActive(true)
+                    setCurrentSymptom(symptom)
+                  }}
+                  small
+                />
+              </Buttons>
+            </td>
+          </tr>
+        ))}
+      </>
     )
+  }
+
+  return (
+    <ZTable
+      tableHeads={tableHeads}
+      page={page}
+      setPage={setPage}
+      tableBody={<TableBody />}
+      paginationInfo={paginationInfo}
+    />
+  )
 }
