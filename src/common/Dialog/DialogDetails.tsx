0f4cb7fc60f2f3cf59fc112a40dce7dc1ed762e1
Ziad Hosny <ziadhosny007@gmail.com>
2024-10-13 16:01:59 +0300

    add dialog details

---
+++ src/common/Dialog/DialogDetails.tsx
@@ -0,0 +1,31 @@
+import React, { Dispatch, SetStateAction } from 'react'
+import CardBoxModal from '../../components/CardBox/Modal'
+
+type Props = {
+  title: string
+  values: object
+  isActive: boolean
+  setIsActive: Dispatch<SetStateAction<boolean>>
+}
+
+export const DialogDetails = ({ title, values, isActive, setIsActive }: Props) => {
+  // Info
+  const handleModalInfoActionCancel = () => {
+    setIsActive(false)
+  }
+
+  return (
+    <CardBoxModal
+      title={title}
+      buttonColor="info"
+      isActive={isActive}
+      onCancel={handleModalInfoActionCancel}
+    >
+      {Object.keys(values).map((e) => (
+        <div key={e} className="w-full">
+          {e} : <span className="font-bold text-blue-500">{values[e]}</span>
+        </div>
+      ))}
+    </CardBoxModal>
+  )
+}
